85737d05bf997a5eabb1560a2667085e92758226
Ben Godfrey <BenJetson@users.noreply.github.com>
2017-10-30 09:45:02 -0400

    Finished practice problems, missing tests.

--- MixedLoopPractice.java
+++ MixedLoopPractice.java
@@ -29,14 +29,14 @@
  *                       (you'll need to use an if statement.)
  *
  * 10) divBy5or7Sum    - uses a while loop to find the sum of all values in the
- *     		         set {1, 2, 3, ... , 10000} that are divisible by 5 or 7.
+ *                   set {1, 2, 3, ... , 10000} that are divisible by 5 or 7.
  *
  * 11) bizzBuzz        - uses a while loop to count from 1 to n by ones. However,
  *                       if a number is divisible by 3, the word "bizz" should be 
- *   	 		 printed, and if the number is divisible by 5, "buzz" should
- * 			 be printed. Sample output:
+ *               printed, and if the number is divisible by 5, "buzz" should
+ *           be printed. Sample output:
  *
- *			 1, 2, Bizz, 4, Buzz, Bizz, 7, 8, Bizz, Buzz, 11, Bizz, 13, 14, 
+ *           1, 2, Bizz, 4, Buzz, Bizz, 7, 8, Bizz, Buzz, 11, Bizz, 13, 14, 
  *                       Bizz Buzz, 16, 17, Bizz, 19, Buzz, Bizz, 22, 23, Bizz, ...
  *
  *
@@ -48,25 +48,104 @@
  
  public class MixedLoopPractice
  {
- 	public static void oneToThirtyF()
- 	{
- 		
- 	}
- 	
- 	public static void oneToThirtyW()
- 	{
- 		
- 	}
- 	    
-	// keep making methods here
+    public static void oneToThirtyF()
+    {
+        for (int i = 1; i <= 30; i++) {
+            System.out.println(i);
+        }
+    }
     
+    public static void oneToThirtyW()
+    {
+        int i = 1;
+        
+        do {
+            System.out.println(i);
+            i++;
+         } while (i <= 30);
+    }
+        
+    // keep making methods here
     
- 	public static void main(String[] args)
- 	{
- 		oneToThirtyF();
- 		oneToThirtyW();
- 		
- 		// test your methods here
- 	}
+    public static void twentyToZeroF() {
+        for (int i = 20; i >= 0; i -= 2) {
+            System.out.println(i);
+        }
+    }
+    
+    public static void twentyToZeroW() {
+        int i = 20;
+        do {
+            System.out.println(i);
+            i -= 2;
+        } while (i >= 0);
+    }
+    
+    public static int sumN(int n) {
+        int sum = 0;
+        for (n = n; n > 0; n--) {
+            sum += n;
+        }
+        System.out.println(sum);
+        return sum;
+    }
+    
+    public static void countMtoN(int m, int n) {
+        for (m = m; m <= n; m++) {
+            System.out.println(m);
+        }
+    }
+    
+    public static void divBy2and9() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0 && i % 9 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+    
+    public static void divBy3and8() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 8 == 0) {
+                System.out.println(i);
+            }
+        }
+    }    
+    
+    public static int divBy5or7Sum() {
+        int sum = 0;
+        for (int i = 1; i <= 10000; i++) {
+            if (i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+        return sum;
+    }
+    
+    public static void bizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            String output = Integer.toString(i);
+            boolean div3 = i % 3 == 0;
+            boolean div5 = i % 5 == 0;
+            if (div3 && div5) {
+                output = "Bizz Buzz";
+            } else if (div3) {
+                output = "Bizz";
+            } else if (div5) {
+                output = "Buzz";
+            }
+            System.out.println(output);
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        oneToThirtyF();
+        oneToThirtyW();
+        
+        bizzBuzz(25);
+        // test your methods here
+    }
  }
  
